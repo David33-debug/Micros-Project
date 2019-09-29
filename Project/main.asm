56eea8b1e7bb56d2fb38d15544120ce7199e59b2
David Curtiss <dcurtiss593@gmail.com>
2019-09-29 18:17:50 +0200

    Game mode works, now for 6 times

--- Project/main.asm
+++ Project/main.asm
@@ -20,7 +20,7 @@ ldi r17,0b00000010
 out tccr0b, r17
 ldi r16,0b11011111
 out ddrb,r16
-ldi r16,0b00010000
+ldi r16,0b11110000
 out ddrd,r16
 ldi r16,0b11110111
 out ddrc, r16
@@ -44,16 +44,20 @@ sei
 jmp sleeping
 
 stay:
+clr r20
 in r24, pinc
 cpi r24, 0b00001000
 breq startTest
-jmp stay
+jmp Modes
 
 Gamestay:
+ldi r28,1
+ldi r20,1
+ldi r29,0
 in r26, pinc
 cpi r26, 0b00001000
 breq play
-jmp Gamestay
+jmp Modes
 
 sleeping:
 sleep
@@ -62,6 +66,8 @@ jmp sleeping
 task:
 cli
 lsr r18
+cpi r20,1
+breq gamePlay
 jmp display
 ret
 
@@ -100,7 +106,65 @@ brne delay1
 ret
 
 play:
-ldi r30,9
-out portb, r30
+ldi r27,0
+out portd, r27
+ldi r27, 0b00000111
+in r24, tcnt0
+and r27, r24
+inc r27
+out portb, r27 
+call delay4S
+jmp startTest
+gamePlay:
+out portb, r18
+cp r27,r18
+breq output2
+cp r27,r18
+brne output1
+
+
+output1:
+cpi r18,10
+brsh output3
+ldi r20,2
+add r29,r20
+ldi r18,0b01000000
+out portd, r18
+jmp Modes
+output2:
+subi r29,1
+ldi r19, 0b10000000
+out portd, r19
+jmp Modes
+output3:
+ldi r19,0b00100000
+out portd, r19
 jmp Modes
 
+delay4S:
+push r16
+push r17
+push r18
+push r19
+loop1:
+ldi r19, 100
+loop2:
+ldi r18,50
+loop3:
+ldi r17,65
+loop4:
+ldi r16, 65
+loop5:
+dec r16
+brne loop5
+dec r17
+brne loop4
+dec r18
+brne loop3
+dec r19
+brne loop2
+pop r19
+pop r18
+pop r17
+pop r16
+ret
